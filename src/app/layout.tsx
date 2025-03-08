6753a7e47117d181d23059e702b7ae3f0325baac
Anh Tri Pham <anhtp5@uci.edu>
2025-03-07 21:44:32 -0800

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Anh-Tri Pham's Portfolio Page",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen antialiased">
         <NavBar />
         <main className="flex-grow">{children}</main>
+        <Analytics />
         <Footer />
       </body>
     </html>
